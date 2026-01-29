96db4eaa7937bb2294755fd9c28b74421063e479
Thilina Dilshan <thilinadilshan626@gmail.com>
2026-01-29 21:21:27 +0530

    feat: implement Add Vehicle page and add styling for Add Driver page.

--- src/pages/vehicles/AddVehicle.jsx
+++ src/pages/vehicles/AddVehicle.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import './AddVehicle.css';
 
 const AddVehicle = () => {
     const navigate = useNavigate();
