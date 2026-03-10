ab0e7144d3011cbdd9ef67fd84397b2d6fe8243a
Thilina Dilshan <thilinadilshan626@gmail.com>
2026-03-10 20:17:43 +0530

    Set base path for Vehicle Management System

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or your specific plugin
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Vehicle-Management-System/',
   plugins: [react()],
 })
